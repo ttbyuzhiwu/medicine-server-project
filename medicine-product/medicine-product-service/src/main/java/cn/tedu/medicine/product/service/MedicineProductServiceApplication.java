01ef2b2094ca971b2ccf6b707d279eb791a9f07b
tang_tianbaower <201736031@qq.com>
2022-04-21 19:39:21 +0800

    项目创建

---
+++ medicine-product/medicine-product-service/src/main/java/cn/tedu/medicine/product/service/MedicineProductServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.medicine.product.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicineProductServiceApplication.class, args);
+    }
+
+}
---
+++ medicine-product/medicine-product-service/src/test/java/cn/tedu/medicine/product/service/MedicineProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.medicine.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
